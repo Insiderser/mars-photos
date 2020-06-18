420d529b8bc50fbb4a1d3a415892e9a0547106bc
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-06-18 13:42:45 +0300

    Display image loading placeholder

--- app/src/main/java/com/insiderser/mars/utils/ImageLoading.kt
+++ app/src/main/java/com/insiderser/mars/utils/ImageLoading.kt
@@ -7,5 +7,6 @@ import com.squareup.picasso.Picasso
 fun ImageView.load(item: MarsImage?) {
     Picasso.get()
         .load(item?.url)
+        .placeholder(android.R.drawable.progress_horizontal)
         .into(this)
 }
