5ca9a9b7a7d3d2b71c782296ad406ce908f30bd0
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-06-19 14:55:51 +0300

    Increase timeout for requests
    Some pages contain as much as ~2500 images.

--- app/src/main/java/com/insiderser/mars/data/remote/NasaImagesWebService.kt
+++ app/src/main/java/com/insiderser/mars/data/remote/NasaImagesWebService.kt
@@ -6,11 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.concurrent.TimeUnit
 
 interface NasaImagesWebService {
 
@@ -29,8 +31,13 @@ object NasaImagesWebServiceModule {
         val moshiConverterFactory = MoshiConverterFactory.create()
         val rxJava2CallAdapterFactory = RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io())
 
+        val client = OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(NasaRemoteConfig.NASA_BASE_URL)
+            .client(client)
             .addConverterFactory(moshiConverterFactory)
             .addCallAdapterFactory(rxJava2CallAdapterFactory)
             .build()
