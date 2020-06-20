08e8853b5163cf2e369d10d62f5f4999f394bc71
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-06-20 11:04:25 +0300

    Use UTC time zone for "now" Sol

--- app/src/main/java/com/insiderser/mars/utils/SolsConfig.kt
+++ app/src/main/java/com/insiderser/mars/utils/SolsConfig.kt
@@ -5,6 +5,7 @@ import com.insiderser.mars.model.isBefore
 import com.insiderser.mars.model.plus
 import java.time.LocalDate
 import java.time.Month
+import java.time.ZoneId
 
 object SolsConfig {
 
@@ -14,13 +15,13 @@ object SolsConfig {
 
     val now: Sol
         get() {
-            val nowOnEarth = LocalDate.now()
+            val utc = ZoneId.of("UTC")
+            val nowOnEarth = LocalDate.now(utc)
             val earthDaysSinceLaunch = daysBetween(landingEarthDate, nowOnEarth)
             val solsSinceLaunch = earthDaysSinceLaunch / 1.02749125
             return Sol(solsSinceLaunch.toInt())
         }
 
     // NASA doesn't provide images immediately.
-    // TODO: test plus days amount. Take into account different Earth time zones.
     fun canFetch(sol: Sol): Boolean = sol.plus(2).isBefore(now)
 }
